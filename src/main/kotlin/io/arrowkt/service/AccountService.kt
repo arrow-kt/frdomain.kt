b22ca0e44ddc516d0cef4035860fba688a43f8b7
Stojan Anastasov <stolea@gmail.com>
2019-04-27 12:55:55 +0200

    Somehow it compiles

--- src/main/kotlin/io/arrowkt/service/AccountService.kt
+++ src/main/kotlin/io/arrowkt/service/AccountService.kt
@@ -1,6 +1,16 @@
 package io.arrowkt.service
 
 import arrow.core.Option
+import arrow.data.EitherT
+import arrow.data.EitherTPartialOf
+import arrow.data.ReaderT
+import arrow.data.extensions.eithert.monad.monad
+import arrow.data.extensions.kleisli.monad.monad
+import arrow.data.fix
+import arrow.effects.ForIO
+import arrow.effects.IO
+import arrow.effects.extensions.io.monad.monad
+import io.arrowkt.repository.AccountRepository
 import java.math.BigDecimal
 import java.time.LocalDate
 
@@ -27,5 +37,10 @@ interface AccountService<Account, Amount, Balance> {
 
     fun balance(no: String): AccountOperation<Balance>
 
-    fun transfer(from: String, to: String, amount: Amount): AccountOperation<Pair<Account, Account>> = TODO()
+    fun transfer(from: String, to: String, amount: Amount): AccountOperation<Pair<Account, Account>> =
+        ReaderT.monad<EitherTPartialOf<ForIO, AccountServiceException>, AccountRepository>(EitherT.monad(IO.monad())).binding {
+            val a = debit(from, amount).bind()
+            val b = credit(to, amount).bind()
+            Pair(a, b)
+        }.fix()
 }
--- src/main/kotlin/io/arrowkt/service/Common.kt
+++ src/main/kotlin/io/arrowkt/service/Common.kt
@@ -1,10 +1,8 @@
 package io.arrowkt.service
 
-import arrow.data.EitherT
+import arrow.data.EitherTPartialOf
 import arrow.data.Kleisli
 import arrow.effects.ForIO
 import io.arrowkt.repository.AccountRepository
 
-typealias Valid<A> = EitherT<ForIO, AccountServiceException, A>
-
-typealias AccountOperation<A> = Kleisli<Valid<A>, AccountRepository, A>
+typealias AccountOperation<A> = Kleisli<EitherTPartialOf<ForIO, AccountServiceException>, AccountRepository, A>
