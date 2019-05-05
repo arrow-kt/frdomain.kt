2674436c35a49341bb5b2c37befa5cf493987ebb
Stojan Anastasov <stolea@gmail.com>
2019-05-05 17:43:49 +0200

    AccountService interpreter

--- src/main/kotlin/io/arrowkt/service/AccountServiceException.kt
+++ src/main/kotlin/io/arrowkt/service/AccountServiceException.kt
@@ -4,8 +4,17 @@ import arrow.data.NonEmptyList
 
 sealed class AccountServiceException(open val message: NonEmptyList<String>)
 
+data class AlreadyExistingAccount(val no: String) :
+    AccountServiceException(NonEmptyList("Already existing account with no $no"))
+
+data class NonExistingAccount(val no: String) :
+    AccountServiceException(NonEmptyList("No existing account with no $no"))
+
 data class ClosedAccount(val no: String) :
     AccountServiceException(NonEmptyList("Account with $no already closed!"))
+
+object RateMissingForSavingsAccount :
+    AccountServiceException(NonEmptyList("Rate needs to be given for savings account"))
 
 data class MiscellaneousDomainExceptions(override val message: NonEmptyList<String>) :
     AccountServiceException(message)
