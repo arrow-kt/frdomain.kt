07fc664f2c1201a38aa73467c470e610629ba49a
Stojan Anastasov <stolea@gmail.com>
2019-05-30 21:11:32 +0200

    Tagless common, model and repo

---
+++ src/main/kotlin/io/arrowkt/tagless/Common.kt
@@ -0,0 +1,13 @@
+package io.arrowkt.tagless
+
+import arrow.core.Either
+import arrow.data.NonEmptyList
+import arrow.data.ValidatedNel
+import java.math.BigDecimal
+import java.time.LocalDate
+
+typealias Amount = BigDecimal
+typealias ValidationResult<A> = ValidatedNel<String, A>
+typealias ErrorOr<A> = Either<NonEmptyList<String>, A>
+
+fun today() = LocalDate.now()
