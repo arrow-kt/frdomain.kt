8c1ce4b7d36c7e3a22c4b2fbdee07d1fd25cffdf
Stojan Anastasov <stolea@gmail.com>
2019-06-02 12:21:23 +0200

    AccountService algebra and interpreter

---
+++ src/main/kotlin/io/arrowkt/tagless/service/AccountService.kt
@@ -0,0 +1,34 @@
+package io.arrowkt.tagless.service
+
+import arrow.core.Option
+import arrow.data.Kleisli
+import arrow.data.fix
+import io.arrowkt.tagless.repository.AccountRepository
+import java.math.BigDecimal
+import java.time.LocalDate
+
+enum class AccountType {
+    CHECKING,
+    SAVINGS
+}
+
+interface AccountService<F, Account, Amount, Balance> {
+
+    fun open(
+        no: String,
+        name: String,
+        rate: Option<BigDecimal>,
+        openingDate: Option<LocalDate>,
+        accountType: AccountType
+    ): Kleisli<F, AccountRepository<F>, Account>
+
+    fun close(no: String, closeDate: Option<LocalDate>): Kleisli<F, AccountRepository<F>, Account>
+
+    fun debit(no: String, amount: Amount): Kleisli<F, AccountRepository<F>, Account>
+
+    fun credit(no: String, amount: Amount): Kleisli<F, AccountRepository<F>, Account>
+
+    fun balance(no: String): Kleisli<F, AccountRepository<F>, Balance>
+
+    fun transfer(from: String, to: String, amount: Amount): Kleisli<F, AccountRepository<F>, Pair<Account, Account>>
+}
